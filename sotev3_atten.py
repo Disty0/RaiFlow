b7f139b261a96317e9cd3e5fa67b5043fa5fa185
Disty0 <disty@disty.xyz>
2025-02-14 01:10:32 +0300

    inital commit

---
+++ sotev3_atten.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn.functional as F
+
+#from diffusers.models.attention_processor import Attention
+from .diffusers_aten import Attention
+
+
+class SoteDiffusionV3AttnProcessor2_0:
+    """Attention processor used in processing the Sote Diffusion V3 self-attention projections."""
+
+    def __init__(self):
+        if not hasattr(F, "scaled_dot_product_attention"):
+            raise ImportError("SoteDiffusionV3AttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
+
+    def __call__(
+        self,
+        attn: Attention,
+        hidden_states: torch.FloatTensor,
+        encoder_hidden_states: torch.FloatTensor = None,
+        *args,
+        **kwargs,
+    ) -> torch.FloatTensor:
+        batch_size, seq_len, _ = hidden_states.shape
+
+        # `sample` projections.
+        query = attn.to_q(hidden_states)
+        key = attn.to_k(hidden_states)
+        value = attn.to_v(hidden_states).clamp(-32768,32768)
+
+        attn_heads = attn.heads
+        head_dim = attn.inner_dim // attn_heads
+
+        query = query.view(batch_size, seq_len, attn_heads, head_dim).transpose(1, 2)
+        key = key.view(batch_size, seq_len, attn_heads, head_dim).transpose(1, 2)
+        value = value.view(batch_size, seq_len, attn_heads, head_dim).transpose(1, 2)
+
+        if attn.norm_q is not None:
+            query = attn.norm_q(query.clamp(-32768,32768))
+        else:
+            query = query.clamp(-255,255)
+        if attn.norm_k is not None:
+            key = attn.norm_k(key.clamp(-32768,32768))
+        else:
+            key = key.clamp(-255,255)
+
+        total_seq_len = seq_len
+        # `context` projections.
+        if encoder_hidden_states is not None:
+            _, encoder_seq_len, _ = encoder_hidden_states.shape
+            total_seq_len = seq_len + encoder_seq_len
+            encoder_hidden_states_query_proj = attn.add_q_proj(encoder_hidden_states)
+            encoder_hidden_states_key_proj = attn.add_k_proj(encoder_hidden_states)
+            encoder_hidden_states_value_proj = attn.add_v_proj(encoder_hidden_states).clamp(-32768,32768)
+
+            encoder_hidden_states_query_proj = encoder_hidden_states_query_proj.view(
+                batch_size, encoder_seq_len, attn_heads, head_dim
+            ).transpose(1, 2)
+            encoder_hidden_states_key_proj = encoder_hidden_states_key_proj.view(
+                batch_size, encoder_seq_len, attn_heads, head_dim
+            ).transpose(1, 2)
+            encoder_hidden_states_value_proj = encoder_hidden_states_value_proj.view(
+                batch_size, encoder_seq_len, attn_heads, head_dim
+            ).transpose(1, 2)
+
+            if attn.norm_added_q is not None:
+                encoder_hidden_states_query_proj = attn.norm_added_q(encoder_hidden_states_query_proj.clamp(-32768,32768))
+            else:
+                encoder_hidden_states_query_proj = encoder_hidden_states_query_proj.clamp(-255,255)
+            if attn.norm_added_k is not None:
+                encoder_hidden_states_key_proj = attn.norm_added_k(encoder_hidden_states_key_proj.clamp(-32768,32768))
+            else:
+                encoder_hidden_states_key_proj = encoder_hidden_states_key_proj.clamp(-255,255)
+
+            query = torch.cat([encoder_hidden_states_query_proj, query], dim=2)
+            key = torch.cat([encoder_hidden_states_key_proj, key], dim=2)
+            value = torch.cat([encoder_hidden_states_value_proj, value], dim=2)
+
+        attn_output = F.scaled_dot_product_attention(query, key, value, dropout_p=0.0, is_causal=False)
+        attn_output = attn_output.transpose(1, 2).reshape(batch_size, total_seq_len, attn_heads * head_dim)
+        attn_output = attn_output.to(query.dtype)
+
+        if encoder_hidden_states is not None:
+            # Split the attention outputs.
+            attn_output, context_attn_output = (
+                attn_output[:, encoder_seq_len :],
+                attn_output[:, : encoder_seq_len],
+            )
+            context_attn_output = attn.to_add_out(context_attn_output)
+
+        # linear proj
+        attn_output = attn.to_out[0](attn_output)
+        # dropout
+        attn_output = attn.to_out[1](attn_output)
+
+        if encoder_hidden_states is not None:
+            return attn_output, context_attn_output
+        else:
+            return attn_output
+
+
+class SoteDiffusionV3CrossAttnProcessor2_0:
+    """Attention processor used in processing the Sote Diffusion V3 self-attention projections."""
+
+    def __init__(self):
+        if not hasattr(F, "scaled_dot_product_attention"):
+            raise ImportError("SoteDiffusionV3CrossAttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
+
+    def __call__(
+        self,
+        attn: Attention,
+        hidden_states: torch.FloatTensor,
+        secondary_hidden_states: torch.FloatTensor,
+        *args,
+        **kwargs,
+    ) -> torch.FloatTensor:
+        batch_size, seq_len, _ = hidden_states.shape
+        _, secondary_seq_len, _ = secondary_hidden_states.shape
+
+        # `sample` projections.
+        query = attn.to_q(hidden_states)
+        key = attn.to_k(secondary_hidden_states)
+        value = attn.to_v(secondary_hidden_states).clamp(-32768,32768)
+
+        attn_heads = attn.heads
+        head_dim = attn.inner_dim // attn_heads
+
+        query = query.view(batch_size, seq_len, attn_heads, head_dim).transpose(1, 2)
+        key = key.view(batch_size, secondary_seq_len, attn_heads, head_dim).transpose(1, 2)
+        value = value.view(batch_size, secondary_seq_len, attn_heads, head_dim).transpose(1, 2)
+
+        if attn.norm_q is not None:
+            query = attn.norm_q(query.clamp(-32768,32768))
+        else:
+            query = query.clamp(-255,255)
+        if attn.norm_k is not None:
+            key = attn.norm_k(key.clamp(-32768,32768))
+        else:
+            key = key.clamp(-255,255)
+
+        attn_output = F.scaled_dot_product_attention(query, key, value, dropout_p=0.0, is_causal=False)
+        attn_output = attn_output.transpose(1, 2).reshape(batch_size, seq_len, attn_heads * head_dim)
+        attn_output = attn_output.to(query.dtype)
+
+        # linear proj
+        attn_output = attn.to_out[0](attn_output)
+        # dropout
+        attn_output = attn.to_out[1](attn_output)
+
+        return attn_output
---
+++ sotev3_embedder.py
@@ -0,0 +1,55 @@
+import torch
+
+from diffusers.utils import logging
+
+logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+
+
+def SoteDiffusionV3PosEmbed1D(embeds: torch.Tensor, sigmas: torch.Tensor, latents_seq_len: int):
+    batch_size, seq_len, _ = embeds.shape
+
+    posed_embeds = torch.ones((batch_size, seq_len, 4), device=embeds.device, dtype=embeds.dtype)
+    posed_embeds[:, :, 0] = torch.linspace(start=0, end=1, steps=seq_len, device=embeds.device, dtype=embeds.dtype)
+    posed_embeds[:, :, 1] = torch.linspace(start=0, end=1, steps=(seq_len + latents_seq_len), device=embeds.device, dtype=embeds.dtype)[:seq_len]
+    posed_embeds[:, :, 2] = posed_embeds[:, :, 2] * sigmas.expand(batch_size, seq_len)
+    posed_embeds[:, :, 3] = posed_embeds[:, :, 3] - sigmas.expand(batch_size, seq_len) # add this to make the dim lenght even
+
+    embeds = torch.cat([embeds, posed_embeds], dim=2)
+    return embeds
+
+
+def SoteDiffusionV3PosEmbed2D(latents):
+    batch_size, _, height, width = latents.shape
+    max_dim = max(width, height)
+    posed_latents = torch.zeros((batch_size, 4, height, width))
+    for x in range(width):
+        posed_latents[:, 0, :, x] = torch.linspace(start=0, end=1, steps=height)
+        posed_latents[:, 1, :, x] = torch.linspace(start=0, end=1, steps=max_dim)[:height]
+    for y in range(height):
+        posed_latents[:, 2, y, :] = torch.linspace(start=0, end=1, steps=width)
+        posed_latents[:, 3, y, :] = torch.linspace(start=0, end=1, steps=max_dim)[:width]
+    posed_latents = posed_latents.to(latents.device, dtype=latents.dtype)
+    posed_latents = torch.cat([latents, posed_latents], dim=1)
+    return posed_latents
+
+
+def SoteDiffusionV3PatchEmbed2D(latents: torch.Tensor, sigmas: torch.Tensor, patch_size: int, embeds_seq_len: int):
+    batch_size, in_channels, height, width = latents.shape
+    patched_latents = latents.reshape(
+        (
+            batch_size,
+            int((width/patch_size) * (height/patch_size)),
+            int(in_channels*patch_size*patch_size)
+        )
+    )
+
+    batch_size, seq_len, channels = patched_latents.shape
+
+    posed_embeds = torch.ones((batch_size, seq_len, 4), device=latents.device, dtype=latents.dtype)
+    posed_embeds[:, :, 0] = torch.linspace(start=0, end=1, steps=seq_len, device=latents.device, dtype=latents.dtype)
+    posed_embeds[:, :, 1] = torch.linspace(start=0, end=1, steps=(seq_len + embeds_seq_len), device=latents.device, dtype=latents.dtype)[embeds_seq_len:]
+    posed_embeds[:, :, 2] = posed_embeds[:, :, 2] * sigmas.expand(batch_size, seq_len)
+    posed_embeds[:, :, 3] = posed_embeds[:, :, 3] - sigmas.expand(batch_size, seq_len) # add this to make the dim lenght even
+
+    patched_latents = torch.cat([patched_latents, posed_embeds], dim=2)
+    return patched_latents
