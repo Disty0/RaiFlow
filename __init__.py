915c1adaee6212e0c0b168ef1cb9233f2ed66db4
Disty0 <disty@disty.xyz>
2025-02-20 20:00:23 +0300

    fix diffusers loading

--- __init__.py
+++ __init__.py
@@ -3,4 +3,9 @@ from .sotev3_embedder import SoteDiffusionV3PosEmbed1D, SoteDiffusionV3PosEmbed2
 from .sotev3_atten import SoteDiffusionV3AttnProcessor2_0, SoteDiffusionV3CrossAttnProcessor2_0 # noqa:F401
 from .sotev3_pipeline_output import SoteDiffusionV3PipelineOutput # noqa:F401
 from .sotev3_pipeline import SoteDiffusionV3Pipeline # noqa:F401
-from .sotev3_image_encoder import SoteV3ImageEncoder  # noqa:F401
+from .sotev3_image_encoder import SoteV3ImageEncoder # noqa:F401
+
+
+# diffusers fails to load the models without these:
+from diffusers.models.modeling_utils import ModelMixin # noqa:F401
+from transformers import ImageProcessingMixin # noqa:F401
